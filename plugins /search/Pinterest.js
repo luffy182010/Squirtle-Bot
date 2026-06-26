6f5690fa0b5220687dccf923a43fbe6ccf97c529
RIOTIX <luffysinbai@gmail.com>
2026-06-26 05:10:45 +0300

    Update newsletter name and jid in Pinterest.js

--- plugins /search/Pinterest.js
+++ plugins /search/Pinterest.js
@@ -32,8 +32,8 @@ async function test(m, { conn, bot, text }) {
       cards: cards,
       mentions: [m.sender],
       newsletter: {
-      name: '𝐕𝐈𝐈7 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️',
-      jid: '120363225356834044@newsletter'
+      name: '𝐑𝐈𝐎 - ᴛɪх',
+      jid: '120363427060972313@newsletter'
     },
     }, reply_status);
     
